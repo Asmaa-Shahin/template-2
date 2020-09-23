05129ef1f1dea9b7cc013a3c039103c44743e359
asmaa-ui <70504333+asmaa-ui@users.noreply.github.com>
2020-09-24 00:22:55 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -24,3 +24,15 @@ $(function(){
         });
     
     });
+$(window).on("load",function(){
+          
+        $(".loading-overlay .spinner").fadeOut(1000,function(){
+            $('body').css("overflow","auto");
+             $(this).parent().fadeOut(1000,function(){
+                 
+                 $(this).remove();
+                 
+                 });
+            
+             });
+            });
